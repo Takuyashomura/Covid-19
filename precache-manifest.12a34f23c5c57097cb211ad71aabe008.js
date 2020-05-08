aea2812bc1249b422b68f10194d638ae721846c7
Takuyashomura <takudai1411@yahoo.co.jp>
2020-05-08 20:32:35 +0900

    Updates

---
+++ precache-manifest.12a34f23c5c57097cb211ad71aabe008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a23f9fea3c40408d46c7f67b98b1882",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "8e154d8d984dff6057b5",
+    "url": "/Covid-19/static/css/main.6ac322f7.chunk.css"
+  },
+  {
+    "revision": "5743b02c5fe78f5ece04",
+    "url": "/Covid-19/static/js/2.9f0a3942.chunk.js"
+  },
+  {
+    "revision": "12ac8af0741afe107c3dcb4ef10e1ce5",
+    "url": "/Covid-19/static/js/2.9f0a3942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e154d8d984dff6057b5",
+    "url": "/Covid-19/static/js/main.3995de8c.chunk.js"
+  },
+  {
+    "revision": "f98418e6d4b04c8b64ea",
+    "url": "/Covid-19/static/js/runtime-main.0f8f1078.js"
+  }
+]);
